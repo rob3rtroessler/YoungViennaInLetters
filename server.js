8e520aeb1ed06afc4f8de6a3bfd5f09562dfb9b4
rob3rtroessler <robertroessler@g.harvard.edu>
2019-03-19 09:40:50 -0400

    test

--- server.js
+++ server.js
@@ -6,41 +6,19 @@ const path = require('path');
 const app = express();
 app.use(express.static('static'));
 
-/*const connection = mysql.createConnection({
-  host     : "letterprojectdata.ckypvlfg0nlw.us-east-1.rds.amazonaws.com",
-  user     : "masteruser",
-  password : "letter4link",
-  port     : "3306"
-});
-
-
-connection.connect(function(err) {
-  if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
-  }
-
-  console.log('Connected to database.');
-});
-
-connection.end();*/
-
-//app.use(express.static('/static', __dirname + '/static'));
-//app.use(express.static(__dirname + '/static/View'));
-//Store all HTML files in view folder.
-//app.use(express.static(__dirname + '/static/Script'));
-//Store all JS and CSS in Scripts folder.
 
 app.get('/',function(req,res){
-
   res.sendFile(__dirname + '/index.html');
-  //It will find and locate index.html from View or Scripts
 });
 
+
+
 let port = process.env.PORT;
+
 if (port == null || port == "") {
   port = 8000;
 }
 app.listen(port);
 
-console.log("Running on heroku");
+// log
+console.log("Running on localhost:8000");
